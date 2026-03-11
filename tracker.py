54523075106406f0ce385ad515eca48224fd80ca
gamehog1387 <47195783+gamehog1387@users.noreply.github.com>
2026-03-11 03:14:20 -0500

    Refactor habit tracking functions and add gym tracking

--- tracker.py
+++ tracker.py
@@ -1,7 +1,23 @@
+import json
+
+def save_data(all_habits):
+    with open("habits.json", "w") as f:
+        json.dump(all_habits, f, indent=4)
+    print("---DATA SAVED TO DISK----")
+
+
+def load_data():
+    filename = "habits.json"
+    try:
+        with open(filename, "r") as f:
+            return json.load(f)
+    except (FileNotFoundError, json.JSONDecodeError):
+        return {}
+    
 def ADD_hours_studying():
     while True:
         try:
-            hours = int(input("Enter Hours studied "))
+            hours = input("Enter Hours studied ")
             struggle = input("What are you struggling with? ")
         except ValueError:
             print("Invalid input")
@@ -12,14 +28,14 @@ def ADD_hours_studying():
         break
     return data
 
-def add_time_gaming():
+def add_time_gym():
     while True:
         try:
-            time_spent = int(input("Time spent gaming?"))
-            was_it_waste = input("Did you waste your time?")
-            plan_tommorow = input("Whats plan for tmr? Are you gaming or nah?")
+            time_spent = input("Time spent at gym? hours/minutes example:2:44 ")
+            was_it_waste = input("Did you waste your time? ")
+            plan_tommorow = input("Whats plan for tmr? Are you going again or resting? ")
         except ValueError:
-            return"Incorrect input"
+            return "Incorrect input"
         data = {
             "time_spent": time_spent,
             "was_it_waste": was_it_waste,
@@ -28,18 +44,22 @@ def add_time_gaming():
         }
         break
     return data
+
         
-studying = {}
-gamingtime = {}
+data = load_data()
+studying = data.get('studying', {})
+gymtime = data.get('gymtime', {})
+save_data
 while True:
 
     print("\nMy Daily habits")
     print("1. ADD - How many hours did you study?")
     print("2. View hours studied")
     print("3. ADD - Time spent at gym?")
-    
+    print("4. View time spent at the gym")
     choice = input("What would you like to ADD? ")
     if choice == '1.' or choice == '1':
+        save_data({"studying": studying, "gymtime": gymtime})
         studying = ADD_hours_studying()
         print("Progress saved")
         
@@ -47,5 +67,11 @@ while True:
         print(f"Good work so far! {studying['hours']} Hours have been spent studying.\nYou are currently struggling with {studying['struggle']}. ")
 
     elif choice == '3' or choice == '3.':
-        gamingtime = add_time_gaming()
-        print("Gaming time has been saved.")
+        save_data({"studying": studying, "gymtime": gymtime})
+        gymtime = add_time_gym()
+        print("Gym time has been saved.")
+    elif choice == '4' or choice == '4.':\
+        print(f"You spent {gymtime['time_spent']} time at the gym, you wasted {gymtime['was_it_waste']} hours:minutes at the gym/nYour plan tommorow is {gymtime['plan_tommorow']}, good luck!")
+       
+       
+        
